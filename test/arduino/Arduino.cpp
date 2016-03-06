335ad8a408c4f8104b0fc31e8ca9ea15115653eb
Lenon Marcel <lenon.marcel@gmail.com>
2016-03-06 03:23:19 -0300

    Create a mock class for Arduino

--- test/arduino/Arduino.cpp
+++ test/arduino/Arduino.cpp
@@ -1,5 +1,19 @@
 #include "Arduino.h"
 
-void pinMode(uint8_t a, uint8_t b) {}
-void digitalWrite(uint8_t a, uint8_t b) {}
-void analogWrite(uint8_t a, int b) {}
+ArduinoMock* instance;
+
+ArduinoMock::ArduinoMock() {
+  instance = this;
+}
+
+void pinMode(uint8_t a, uint8_t b) {
+  instance->pinMode(a, b);
+}
+
+void digitalWrite(uint8_t a, uint8_t b) {
+  instance->digitalWrite(a, b);
+}
+
+void analogWrite(uint8_t a, int b) {
+  instance->analogWrite(a, b);
+}
--- test/arduino/Arduino.h
+++ test/arduino/Arduino.h
@@ -1,10 +1,20 @@
 #include <stdlib.h>
+#include <gmock/gmock.h>
 
 #define HIGH 0x1
 #define LOW 0x0
 
 #define INPUT 0x0
 #define OUTPUT 0x1
+
+class ArduinoMock
+{
+  public:
+    ArduinoMock();
+    MOCK_METHOD2(pinMode, void(uint8_t, uint8_t));
+    MOCK_METHOD2(digitalWrite, void(uint8_t, uint8_t));
+    MOCK_METHOD2(analogWrite, void(uint8_t, int));
+};
 
 extern "C" {
   void pinMode(uint8_t, uint8_t);
