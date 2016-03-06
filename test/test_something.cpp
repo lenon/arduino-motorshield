435d2ae4a2a25be05ed5cac3fde80a0295867e95
Lenon Marcel <lenon.marcel@gmail.com>
2016-03-06 02:10:17 -0300

    Add a test for MotorShield constructor

--- test/test_something.cpp
+++ test/test_something.cpp
@@ -1,8 +1,21 @@
 #include "gtest/gtest.h"
 #include "motor_shield.h"
 
-MotorShield mshield(1, 3, 4, 5);
+#define SPEED_L 1
+#define DIR_L 2
+
+#define SPEED_R 3
+#define DIR_R 4
+
+MotorShield mshield(SPEED_L, DIR_L, SPEED_R, DIR_R);
 
 TEST(MotorShield, Initialization) {
-  ASSERT_TRUE(1 == 1);
+  ASSERT_EQ(0, mshield.getMinSpeed());
+  ASSERT_EQ(255, mshield.getMaxSpeed());
+
+  ASSERT_EQ(SPEED_L, mshield.getLeftSpeedPin());
+  ASSERT_EQ(SPEED_R, mshield.getRightSpeedPin());
+
+  ASSERT_EQ(DIR_L, mshield.getLeftDirPin());
+  ASSERT_EQ(DIR_R, mshield.getRightDirPin());
 }
